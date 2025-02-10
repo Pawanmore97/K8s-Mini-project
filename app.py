3b3946f0101304cbadabe764eea37cdb2232a1a2
Pawan More <pawan.more089@gmail.com>
2025-02-10 22:40:54 +0530

    changes made in docker file

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def welcome_msg():
     return render_template("index.html", name=None)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",port=5000,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8-alpine
+FROM python:3.8-slim
 
 COPY . /app
 
@@ -8,4 +8,6 @@ RUN pip install -r requirements.txt
 
 EXPOSE 5000
 
-CMD python3 app.py
+ENV FALSK_APP = app.py
+
+CMD ["python", "app.py"]
